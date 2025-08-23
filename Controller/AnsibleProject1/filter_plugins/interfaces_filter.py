4d63363535e9ab1c112f990612062503de11298c
pajeanbe <157420270+pajeanbe@users.noreply.github.com>
2025-08-23 16:05:22 -0400

    test

--- Controller/AnsibleProject1/filter_plugins/interfaces_filter.py
+++ Controller/AnsibleProject1/filter_plugins/interfaces_filter.py
@@ -1,29 +1,77 @@
-def get_unused_interfaces(interface_data):
-    """
-    Parses 'show interfaces', with stdout_lines ,output and returns a list of interfaces
-    with 0 input and 0 output packets.
-    """
- #   un_used=[]
-   # for line in interface_data:
+import re
 
+def find_unused_interfaces_on_cisco_nodes(data):
+    unused_interfaces = []
+    current_interface = None
+    buffer = []
 
+    for line in data:
+        # Detect interface line
+        if re.match(r'^\s*(Ethernet|FastEthernet|GigabitEthernet|TenGigabitEthernet|Vlan)\d+(/\d+)*', line):
+            if current_interface and buffer:
+                if is_unused(buffer):
+                    unused_interfaces.append(current_interface)
+            current_interface = line.strip()
+            buffer = []
+        else:
+            buffer.append(line.strip())
 
+    # Check last interface
+    if current_interface and buffer:
+        if is_unused(buffer):
+            unused_interfaces.append(current_interface)
 
-    unused = []
-    for block in interface_data.split('\n\n'):
-        lines = block.strip().splitlines()
-        if not lines:
-            continue
-        name_line = lines[0]
-        ifname = name_line.split()[0]
-        input_line = next((l for l in lines if 'packets input' in l), '')
-        output_line = next((l for l in lines if 'packets output' in l), '')
-        if 'packets input 0' in input_line and 'packets output 0' in output_line:
-            unused.append(ifname)
-    return unused
+    return unused_interfaces
+
+def is_unused(lines):
+    for line in lines:
+        if line.startswith("Total"):
+            # Extract packet counts
+            numbers = re.findall(r'\d+', line)
+            if len(numbers) >= 4:
+                pkts_in, pkts_out = int(numbers[0]), int(numbers[2])
+                return min(pkts_in,pkts_out)==0
+    return False
 
 class FilterModule(object):
     def filters(self):
         return {
-            'get_unused_interfaces': get_unused_interfaces
+            'find_unused_interfaces_on_cisco_nodes': find_unused_interfaces_on_cisco_nodes
         }
+
+
+
+"""
+# Example usage
+data = [
+    "Ethernet0/0",
+    "          Switching path    Pkts In   Chars In   Pkts Out  Chars Out",
+    "               Processor      15164    2010806      66427    7033162",
+    "             Route cache          0          0          0          0",
+    "                   Total      15164    2010806      66427    7033162",
+    "Ethernet0/1",
+    "          Switching path    Pkts In   Chars In   Pkts Out  Chars Out",
+    "               Processor          0          0      48120    3557499",
+    "             Route cache          0          0          0          0",
+    "                   Total          0          0      48120    3557499",
+    "Ethernet0/2",
+    "          Switching path    Pkts In   Chars In   Pkts Out  Chars Out",
+    "               Processor          0          0      48119    3557059",
+    "             Route cache          0          0          0          0",
+    "                   Total          0          0      48119    3557059",
+    "Ethernet0/3",
+    "          Switching path    Pkts In   Chars In   Pkts Out  Chars Out",
+    "               Processor          0          0      48114    3559000",
+    "             Route cache          0          0          0          0",
+    "                   Total          0          0      48114    3559000",
+    "Vlan1",
+    "          Switching path    Pkts In   Chars In   Pkts Out  Chars Out",
+    "               Processor      12302     847713      18493    3489683",
+    "             Route cache          0          0          0          0",
+    "                   Total      12302     847713      18493    3489683"
+]
+
+unused = find_unused_interfaces(data)
+print("Unused interfaces:", unused)
+
+"""
